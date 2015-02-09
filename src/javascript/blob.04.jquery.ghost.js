c029972944f975769ad33a721648fd19d9b588a7
Aglezabad <aglezabad@gmail.com>
2015-02-09 19:07:41 +0100

    Update 9/2 19:08

--- src/javascript/blob.04.jquery.ghost.js
+++ src/javascript/blob.04.jquery.ghost.js
@@ -7,7 +7,7 @@
     window.Ghost = {
         isAuthenticated: function () {
             var session = getLocalStorage(sessionKey);
-            return session && new Date < new Date(session.expires_at);
+            return session && new Date() < new Date(session.expires_at);
         },
         edit: function (postId) {
             window.location.href = "/ghost/editor/" + postId;
